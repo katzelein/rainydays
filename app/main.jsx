61413d33aba7c029edf8fe3639c2376d3bbc256c
Kat Guthrie <kathrynguthrie@gmail.com>
2016-11-10 18:12:51 -0500

    set up material-UI

--- app/main.jsx
+++ app/main.jsx
@@ -7,28 +7,45 @@ import {connect, Provider} from 'react-redux'
 import store from './store'
 
 // import Jokes from './components/Jokes'
-// import Login from './components/Login'
+import Login from './components/Login'
 // import GoogleLogin from './components/GoogleLogin'
-// import WhoAmI from './components/WhoAmI'
+import WhoAmI from './components/WhoAmI'
 
-// const App = connect(
-//   ({ auth }) => ({ user: auth })
-// ) (
-//   ({ user, children }) =>
-//     <div>
-//       <nav>
-//         {user ? <WhoAmI/> : <Login/>}
-//       </nav> 
-//       {children}
-//     </div>
-// )
+const App = connect(
+  ({ auth }) => ({ user: auth })
+) (
+  ({ user, children }) =>
+    <div>
+      <nav>
+        {user ? <WhoAmI/> : <Login/>}
+      </nav> 
+      {children}
+    </div>
+)
+
+// for Google's Material UI
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { yellow300, blue900 } from 'material-ui/styles/colors';
+
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: yellow300,
+    primary2Color: blue900,
+  },
+});
 
 render (
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-      </Route>
-    </Router>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('main')
 )
